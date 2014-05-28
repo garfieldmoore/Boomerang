0d6ec1afcbafb79925f95f8a9a4a1a12cd55a919
garfieldmoore <garfieldmoore@gmail.com>
2014-05-29 00:09:13 +0100

    changed nuget specification to include repeat extensions

--- source/Boomerang.Extensions/BoomerangExtensions.cs
+++ source/Boomerang.Extensions/BoomerangExtensions.cs
@@ -5,11 +5,20 @@ namespace Boomerang.Extensions
 {
     public static class BoomerangExtensions
     {
-        public static IBoomerang Repeat(this IBoomerang host, Action<IBoomerang> func, int n)
+        /// <summary>
+        /// Configure actions n times
+        /// </summary>
+        /// <param name="host">Boomerang instance</param>
+        /// <param name="configuration">The configuration as a lamda funciton</param>
+        /// <param name="n">The number of times to repeat the configuration</param>
+        /// <returns>The host instance</returns>
+        /// <example>The below code configures 10 GET responses for http://localhost:5100/test
+        /// <code>Boomerang.Server(5100).Repeat(x => x.Get("test").Returns("response"), 10)</code></example>
+        public static IBoomerang Repeat(this IBoomerang host, Action<IBoomerang> configuration, int n)
         {
             for (int i = 0; i < n; i++)
             {
-                func(host);
+                configuration(host);
             }
 
             return host;
