eb984ece1ab54bcc99be7e7ae0ebb2c7a227c062
garfieldmoore <garfieldmoore@gmail.com>
2014-10-04 17:17:36 +0100

    Added repository that is a list with one response per address.

---
+++ source/boomerang.host/SingleResponseRepository.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+
+namespace Rainbow.Testing.Boomerang.Host
+{
+    public class SingleResponseRepository : IResponseRepository
+    {
+        public IDictionary<Request, Response> RegisteredAddresses = new Dictionary<Request, Response>();
+        private Request previouseRequest;
+
+        public IEnumerable<Queue<Response>> Requests()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddAddress(Request request)
+        {
+            previouseRequest = request;
+
+            if (!RegisteredAddresses.ContainsKey(request))
+            {
+                RegisteredAddresses.Add(new KeyValuePair<Request, Response>(request, null));
+            }
+        }
+
+        public void AddResponse(string body, int statusCode)
+        {
+            if (RegisteredAddresses.ContainsKey(previouseRequest))
+            {
+                Response response = null;
+                RegisteredAddresses.TryGetValue(previouseRequest, out response);
+                RegisteredAddresses.Remove(new KeyValuePair<Request, Response>(previouseRequest, response));
+                RegisteredAddresses.Add(new KeyValuePair<Request, Response>(previouseRequest, new Response() { Body = body, StatusCode = statusCode }));
+            }
+        }
+
+        public void AddResponse(string body, int statusCode, IDictionary<string, string> headers)
+        {
+            if (RegisteredAddresses.ContainsKey(previouseRequest))
+            {
+                Response response = null;
+                RegisteredAddresses.TryGetValue(previouseRequest, out response);
+                RegisteredAddresses.Remove(new KeyValuePair<Request, Response>(previouseRequest, response));
+                RegisteredAddresses.Add(new KeyValuePair<Request, Response>(previouseRequest, new Response() { Body = body, StatusCode = statusCode, Headers = headers }));
+            }
+        }
+
+        public bool Contains(Request request)
+        {
+            return RegisteredAddresses.ContainsKey(request);
+        }
+
+        public int GetCount()
+        {
+            return RegisteredAddresses.Count;
+        }
+
+        public bool GetAllResponsesFor(Request request, out Queue<Response> req)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Response GetNextResponseFor(string method, string addressTarget)
+        {
+            Response response=null;
+            RegisteredAddresses.TryGetValue(new Request() {Address = addressTarget, Method = method}, out response);
+
+            return response;
+        }
+    }
+}
---
+++ tests/boomerang.tests.unit/SingleResponseRepositorySpecs.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using Rainbow.Testing.Boomerang.Host;
+using Shouldly;
+
+namespace boomerang.tests.unit
+{
+    public class SingleResponseRepositorySpecs
+    {
+        private SingleResponseRepository _repository;
+
+        [Test]
+        public void Contains_returns_false_if_request_not_added()
+        {
+            GivenASingleResponseRepository();
+            _repository.Contains(new Request()).ShouldBe(false);
+        }
+
+        [Test]
+        public void Should_return_response()
+        {
+            var repository = new SingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+            repository.AddAddress(request);
+
+            repository.Contains(request).ShouldBe(true);
+        }
+
+        [Test]
+        public void Count_returns_number_of_added_requests()
+        {
+            GivenASingleResponseRepository();
+            _repository.AddAddress(new Request());
+
+            _repository.GetCount().ShouldBe(1);
+        }
+
+        [Test]
+        public void Should_return_two_when_two_requests_added()
+        {
+            GivenASingleResponseRepository();
+
+            _repository.AddAddress(Builder<Request>.CreateNew().Build());
+            _repository.AddAddress(Builder<Request>.CreateNew().With(x => x.Method = "GET").Build());
+
+            _repository.GetCount().ShouldBe(2);
+        }
+
+        [Test]
+        public void AddResponse_adds_response_to_address()
+        {
+            GivenASingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+            _repository.AddAddress(request);
+
+            _repository.AddResponse("my body", 202);
+
+            _repository.RegisteredAddresses.Contains(new KeyValuePair<Request, Response>(request,
+                new Response() { Body = "my body", StatusCode = 202 }));
+        }
+
+        [Test]
+        public void Adding_same_addresses_again_does_not_add_another_adress()
+        {
+            GivenASingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+            _repository.AddAddress(request);
+            _repository.AddAddress(request);
+
+            _repository.RegisteredAddresses.Count.ShouldBe(1);
+        }
+
+        [Test]
+        public void Adding_multiple_responses_replaces_previous()
+        {
+            GivenASingleResponseRepository();
+            var request = Builder<Request>.CreateNew().Build();
+            _repository.AddAddress(request);
+
+            _repository.AddResponse("r1", 404);
+            _repository.AddResponse("r2", 201);
+
+            Response response;
+            _repository.RegisteredAddresses.TryGetValue(request, out response);
+
+            response.Body.ShouldBe("r2");
+            response.StatusCode.ShouldBe(201);
+        }
+
+        [Test]
+        public void AddResponse_replace_previous_add_attempt_when_adding_same_address_multiple_times()
+        {
+            GivenASingleResponseRepository();
+            var request1 = Builder<Request>.CreateNew().With(x => x.Address = "address2").Build();
+            var request2 = Builder<Request>.CreateNew().With(x => x.Method = "new").Build();
+            var response = Builder<Response>.CreateNew().Build();
+
+            _repository.AddAddress(request1);
+            _repository.AddAddress(request2);
+            _repository.AddAddress(request1);
+
+            _repository.AddResponse(response.Body, response.StatusCode);
+
+            Response registeredResponse;
+            _repository.RegisteredAddresses.TryGetValue(request1, out registeredResponse);
+
+            registeredResponse.Body.ShouldBe(response.Body);
+            registeredResponse.StatusCode.ShouldBe(response.StatusCode);
+        }
+
+        [Test]
+        public void AddResponse_with_headers_adds_headers()
+        {
+            GivenASingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+
+            _repository.AddAddress(request);
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("k1", "v1");
+            _repository.AddResponse("r1", 200, dictionary);
+
+            Response response;
+            _repository.RegisteredAddresses.TryGetValue(request, out response);
+
+            response.Headers.Count.ShouldBe(1);
+        }
+
+        [Test]
+        public void GetNextResponse_returns_response_for_address()
+        {
+            GivenASingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+            _repository.AddAddress(request);
+            _repository.AddResponse("b1", 201);
+
+            var response = _repository.GetNextResponseFor(request.Method, request.Address);
+
+            response.StatusCode.ShouldBe(201);
+            response.Body.ShouldBe("b1");
+        }
+
+        [Test]
+        public void GetNextResponse_invoked_multiple_times_returns_same_response()
+        {
+            GivenASingleResponseRepository();
+
+            var request = Builder<Request>.CreateNew().Build();
+            _repository.AddAddress(request);
+            _repository.AddResponse("b1", 201);
+
+            var response = _repository.GetNextResponseFor(request.Method, request.Address);
+            response = null;
+            response = _repository.GetNextResponseFor(request.Method, request.Address);
+
+            response.StatusCode.ShouldBe(201);
+            response.Body.ShouldBe("b1");
+        }
+
+        private void GivenASingleResponseRepository()
+        {
+            _repository = new SingleResponseRepository();
+        }
+    }
+}
